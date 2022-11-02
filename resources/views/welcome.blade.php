d7ea6b023fa0ae9f54f49673a6e23cc7316bdf02
Alvin Mwakai <mwakaimwambala@gmail.com>
2022-11-02 16:09:40 +0300

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,37 @@
 @extends('layouts.frontend')
 @section('content')
 
+<form action="" method="POST">
+    <div class="container">
+        <div class="col-md-8">
+            <label for="exampleFormControlInput1" class="form-label">Name</label>
+            <input type="text" class="form-control" id="name" placeholder="Name">
+        </div>
+        <div class="col-md-8">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="email" placeholder="email@gmail.com">
+        </div>
+        <div class="col-md-8">
+            <label for="exampleFormControlInput1" class="form-label">Title</label>
+            <input type="text" class="form-control" id="title" placeholder="Title">
+        </div>
+        <div class="col-md-8">
+            <label for="exampleFormControlTextarea1" class="form-label">Issue</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" placeholder="Describe your issue"></textarea>
+        </div>
+        <div class="col-md-8">
+        <label for="formFile" class="form-label">Default file input example</label>
+        <input class="form-control" type="file" id="formFile">
+        </div>
+        <button type="submit" class="button">
+            Send
+        </button>
+    </div>
+
+</form>
+
+
+
 
 
 @endsection
