3b26cc0e66e6cdc760cd7be7bf647ef8b7277e7f
Alvin Mwakai <mwakaimwambala@gmail.com>
2022-11-03 09:40:33 +0300

    init

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
+
+    protected $table='tickets';
+    protected $fillable=[
+        'name',
+        'email',
+        'title',
+        'issue',
+        'image'
+
+    ];
 }
