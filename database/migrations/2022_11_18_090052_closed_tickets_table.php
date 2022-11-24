a371f4c7bde6d61e996ece693e44e06661bbbe2e
Alvin Mwakai <mwakaimwambala@gmail.com>
2022-11-24 09:24:11 +0300

    init

--- database/migrations/2022_11_18_090052_closed_tickets_table.php
+++ database/migrations/2022_11_18_090052_closed_tickets_table.php
@@ -14,6 +14,15 @@ return new class extends Migration
     public function up()
     {
         //
+        Schema::create('closed_tickets', function(Blueprint $table){
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->unique();
+            $table->string('title');
+            $table->text('issue');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -24,5 +33,6 @@ return new class extends Migration
     public function down()
     {
         //
+        Schema::dropIfExists('closed_tickets');
     }
 };
