04b7786fa30ca9fbb3e6d95c2d735b7a5b109eb4
Alvin Mwakai <mwakaimwambala@gmail.com>
2022-12-02 13:08:35 +0300

    init

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Ticket;
 
 class ReplyController extends Controller
 {
@@ -10,6 +11,7 @@ class ReplyController extends Controller
 
     public function reply() {
 
+        
         return view('admin.reply');
     }
 }
